f268591ba67a3b7adcedec7e696c1c5d59719090
AtaeTL <ataetlidii@gmail.com>
2022-12-08 03:24:07 -0800

    First print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-/** 
- * main - Prints out a string
- * Return: 0 if success. 
- */
+/**
+ *
+ *  * Description: main - prints out a string.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
