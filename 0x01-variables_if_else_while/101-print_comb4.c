7a4c1629b3f7a76a4ec80fed9ff772d4a203b1c1
AtaeTL <ataetlidii@gmail.com>
2022-12-09 11:20:47 +0100

    All possible combinations of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description : All possible combinations of single-digit numbers.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n, i, k;
+for (n = '0'; n <= '9'; n++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (n < i && i < k)
+{
+putchar(n);
+putchar(i);
+putchar(k);
+if (n != '7')
+{
+putchar(',');				
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
