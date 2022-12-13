cd0de20a8c7dda3b23b354ed3208db60f409d639
AtaeTL <ataetlidii@gmail.com>
2022-12-13 16:53:50 +0100

    Check if a character is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-*_isalpha - Checks if a character is alpha.
-* @x : character's checker                                                      * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+* _isalpha - Checks if a character is alpha.
+* @x : character's checker
+* Return: 1 if x is a letter, lowercase or uppercase, 0 otherwise.
 */
 int _isalpha(int x)
 {
