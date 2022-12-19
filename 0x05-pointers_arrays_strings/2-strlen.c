1b6a3886e1988a4a67f61c775087572d47efe354
AtaeTL <ataetlidii@gmail.com>
2022-12-19 13:24:44 +0100

    Length of an input

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _strlen - Function that returns the length of a string
+* @s: Input
+* Return: Length of the string
+*/
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0' ; i++) ;
+return (index);
+}
