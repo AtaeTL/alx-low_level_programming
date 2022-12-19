48d312f4dd5e25b16bcf454f4d1b8258de3f8349
AtaeTL <ataetlidii@gmail.com>
2022-12-19 13:46:58 +0100

    Pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* rev_string - This fuction print a string reverser
-*@s: this is the pointer that point to a string
+* rev_string - this fuction print a string reverser
+* Description: reversing a string
+* @s: this is the pointer that point to a string
 */
 void rev_string(char *s)
 {
