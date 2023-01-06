7da697f0ad996ab74310ee6c9cf941fe19f22665
AtaeTL <ataetlidii@gmail.com>
2023-01-06 17:57:01 +0100

    Last update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,7 +14,7 @@ int num, j, result;
 int coins[] = {25, 10, 5, 2, 1};
 if (argc != 2)
 {
-printf("Error\n");		
+printf("Error\n");
 return (1);
 }
 num = atoi(argv[1]);
@@ -30,7 +30,7 @@ while (num >= coins[j])
 {
 result++;
 num -= coins[j];
-}		
+}
 }
 printf("%d\n", result);
 return (0);
