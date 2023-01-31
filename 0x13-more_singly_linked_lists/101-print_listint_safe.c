e4f40b5690d1184f8d34631fd8c5928e50826467
AtaeTL <ataetlidii@gmail.com>
2023-01-31 16:27:45 +0100

    ok

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,30 @@
 #include "lists.h"
+
 /**
-* print_listint_safe - prints a list but safely
-* @head: the head node. pointer
-* Description: dont wanna print a infinite loop
-* either match addresses or free the head so cant loop back
-* Return: the count number
-*/
-size_t print_listint_safe(const listint_t *head)
+ * free_listint_safe - prints a list
+ * @h: address of pointer to first node
+ *
+ * Return: address of head
+ */
+size_t free_listint_safe(listint_t **h)
 {
-size_t count = 0;
-size_t index = 0;
-listint_t const **array;
-array = malloc(sizeof(listint_t *) * 1024);
-if (!array)
-exit(98);
-unsigned int i = 0;
-unsigned int flag = 0;
-while (head != NULL)
-{
-for (i = 0; i < count; i++)
-{
-if (head == array[i])
-{
-flag = 1;
-index = i;
-break;
-}
-else
-flag = 0;
-}
-if (flag == 1)
-break;
-array[count] = head;
-head = head->next;
-count++;
-}
-i = 0;
-while (i < count)
-{
-printf("[%p] %d\n", (void *)array[i], array[i]->n);
-i++;
-}
-if (flag == 1)
-{
-printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
-}
-free(array);
-return (count);
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
+
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
+	{
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
+	}
+	return (c);
 }
