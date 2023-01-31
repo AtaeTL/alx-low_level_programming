abb2a91b161c3767a280240003373a856aa1c4ab
AtaeTL <ataetlidii@gmail.com>
2023-01-31 16:48:45 +0100

    Update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,44 +1,30 @@
 #include "lists.h"
+
 /**
-* free_listint_safe - frees a list
-* @h: the pointer to a pointer of head
-* Description: Task 
-* Return: the size of the list
-*/
+ * free_listint_safe - prints a list
+ * @h: address of pointer to first node
+ *
+ * Return: address of head
+ */
 size_t free_listint_safe(listint_t **h)
 {
-size_t count = 0;
-listint_t **array;
-unsigned int i = 0;
-unsigned int flag = 0;
-array = malloc(sizeof(listint_t *) * 1024);
-if (!array)
-exit(98);
-while (*h != NULL)
-{
-for (i = 0; i < count; i++)
-{
-if (*h == array[i])
-{
-flag = 1;
-break;
-}
-else
-flag = 0;
-}
-if (flag == 1)
-break;
-array[count] = *h;
-*h = (*h)->next;
-count++;
-}
-i = 0;
-while (i < count)
-{
-free(array[i]);
-i++;
-}
-free(array);
-*h = NULL;
-return (count);
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
+
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
+	{
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
+	}
+	return (c);
 }
